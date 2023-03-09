9977dcb7ea3a1d8f261dbd15d7fcd75debd137b4
imberbesi <jlberbessi@gmail.com>
2023-03-09 17:31:28 -0500

    Add card details array Javascript

--- script.js
+++ script.js
@@ -51,3 +51,44 @@ forBtn.addEventListener('click', () => {
   bodyCo.style.overflow = 'auto';
   nav.style.width = 'auto';
 });
+
+* Works Session */
+
+worksCardArr = [
+  {
+    Id: 1,
+    Svg: '<svg   class="d-mob"   width="342"   height="492"   viewBox="0 0 329 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="0.5"     y="0.5"     width="328"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg> <svg   class="d-desk"   width="370"   height="476"   viewBox="0 0 370 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="-0.5"     y="0.5"     width="370"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg>',
+    Headding: "Multi-Post Stories Gain+Glory",
+    Languages: ["Ruby on rails", "CSS", "JavaScript", "html"],
+  },
+  {
+    Id: 2,
+    Svg: '<svg   class="d-mob"   width="342"   height="492"   viewBox="0 0 329 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="0.5"     y="0.5"     width="328"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg> <svg   class="d-desk"   width="370"   height="476"   viewBox="0 0 370 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="-0.5"     y="0.5"     width="370"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg>',
+    Headding: "Multi-Post Stories Gain+Glory",
+    Languages: ["Ruby on rails", "CSS", "JavaScript", "html"],
+  },
+  {
+    Id: 3,
+    Svg: '<svg   class="d-mob"   width="342"   height="492"   viewBox="0 0 329 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="0.5"     y="0.5"     width="328"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg> <svg   class="d-desk"   width="370"   height="476"   viewBox="0 0 370 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="-0.5"     y="0.5"     width="370"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg>',
+    Headding: "Multi-Post Stories Gain+Glory",
+    Languages: ["Ruby on rails", "CSS", "JavaScript", "html"],
+  },
+  {
+    Id: 4,
+    Svg: '<svg   class="d-mob"   width="342"   height="492"   viewBox="0 0 329 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="0.5"     y="0.5"     width="328"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg> <svg   class="d-desk"   width="370"   height="476"   viewBox="0 0 370 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="-0.5"     y="0.5"     width="370"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg>',
+    Headding: "Multi-Post Stories Gain+Glory",
+    Languages: ["Ruby on rails", "CSS", "JavaScript", "html"],
+  },
+  {
+    Id: 5,
+    Svg: '<svg   class="d-mob"   width="342"   height="492"   viewBox="0 0 329 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="0.5"     y="0.5"     width="328"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg> <svg   class="d-desk"   width="370"   height="476"   viewBox="0 0 370 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="-0.5"     y="0.5"     width="370"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg>',
+    Headding: "Multi-Post Stories Gain+Glory",
+    Languages: ["Ruby on rails", "CSS", "JavaScript", "html"],
+  },
+  {
+    Id: 6,
+    Svg: '<svg   class="d-mob"   width="342"   height="492"   viewBox="0 0 329 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="0.5"     y="0.5"     width="328"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg> <svg   class="d-desk"   width="370"   height="476"   viewBox="0 0 370 476"   fill="none"   xmlns="http://www.w3.org/2000/svg" >   <rect     x="-0.5"     y="0.5"     width="370"     height="475"     rx="8.5"     fill="#EBF0EE"     stroke="#D0D9D4"   /> </svg>',
+    Headding: "Multi-Post Stories Gain+Glory",
+    Languages: ["Ruby on rails", "CSS", "JavaScript", "html"],
+  },
+];
